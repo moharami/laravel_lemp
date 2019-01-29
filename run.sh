0cbd986502dbeee2fbd6875a208741005593375b
amir <a.moharami@gmail.com>
2019-01-29 12:48:35 +0330

    add composer for install laravel

--- run.sh
+++ run.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
-Dir="laravel"
-if [ -d $Dir ]
+project="laravel"
+if [ -d $project ]
 then
     echo "Directory already exists"
     docker-compose up --build -d
 else	
-    git clone https://github.com/laravel/laravel.git $Dir
-    cd  $Dir    
-    docker run --rm -v $(pwd):/app composer install
-    cp .env.example .env
-    cd ..
-    sudo chown -R $USER:$USER $Dir
+    docker run --rm -v $(pwd):/app composer create-project laravel/laravel $project --prefer-dist
     docker-compose up --build -d
     docker-compose exec app php artisan key:generate
 fi
-
-
