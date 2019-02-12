632aee822309a422d2c5e49c6516bbddd36f0a1c
amir <a.moharami@gmail.com>
2019-02-12 20:32:18 +0330

    add permision

--- run.sh
+++ run.sh
@@ -8,4 +8,10 @@ else
     docker run --rm -v $(pwd):/app composer create-project laravel/laravel $project --prefer-dist
     docker-compose up --build -d
     docker-compose exec app php artisan key:generate
+    docker-compose exec sudo chown -R $USER:www-data storage
+    docker-compose exec sudo chown -R $USER:www-data bootstrap/cache
+    docker-compose exec chmod -R 775 storage
+    docker-compose exec chmod -R 775 bootstrap/cache
+
+
 fi
