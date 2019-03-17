64de63525a409b88ba47bdd5a98dae173a4a91f4
Amir Moharami <a.moharami@gmail.com>
2019-03-17 13:19:54 +0330

    edit readme

--- run.sh
+++ run.sh
@@ -5,13 +5,12 @@ then
     echo "Directory already exists"
     docker-compose up --build -d
 else	
-    docker run --rm -v $(pwd):/app composer create-project laravel/laravel $project --prefer-dist
+    cp sample.env .env
+    # docker run --rm -v $(pwd):/app composer create-project laravel/laravel ./projects/$project --prefer-dist
     docker-compose up --build -d
-    docker-compose exec app php artisan key:generate
-    docker-compose exec sudo chown -R $USER:www-data storage
-    docker-compose exec sudo chown -R $USER:www-data bootstrap/cache
-    docker-compose exec chmod -R 775 storage
-    docker-compose exec chmod -R 775 bootstrap/cache
-
-
+    # docker-compose exec app php artisan key:generate
+    # docker-compose exec sudo chown -R $USER:www-data storage
+    # docker-compose exec sudo chown -R $USER:www-data bootstrap/cache
+    # docker-compose exec chmod -R 775 storage
+    # docker-compose exec chmod -R 775 bootstrap/cache
 fi
